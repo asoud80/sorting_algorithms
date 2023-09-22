2079991fd2b55686ed1de612082c0843eb3a6d11
asoud80 <amgad_a.soud@outlook.com>
2023-09-22 09:09:32 -0700

    106-bitonic_sort.c

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,115 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap - that's to Change 2 Values In Ascending Or Descending Order.
+ *
+ * @arr: an array.
+ *
+ * @item1: Item number One.
+ *
+ * @item2: Item number Two.
+ *
+ * @order: 0 Descending Order, 1: an ascending Order,
+ */
+
+void swap(int arr[], int item1, int item2, int order)
+{
+	int temp;
+
+	if (order == (arr[item1] > arr[item2]))
+	{
+		temp = arr[item1];
+		arr[item1] = arr[item2];
+		arr[item2] = temp;
+	}
+}
+
+/**
+ * merge - to Sort Bitonic Sequences Recursively In Both Orders.
+ *
+ * @arr: an array.
+ *
+ * @low: 1st Element.
+ *
+ * @nelemnt: the Elements Number.
+ *
+ * @order: 0 Descending Order, 1: an ascending Order,
+ */
+
+void merge(int arr[], int low, int nelemnt, int order)
+{
+	int mid, i;
+
+	if (nelemnt > 1)
+	{
+		mid = nelemnt / 2;
+		for (i = low; i < low + mid; i++)
+			swap(arr, i, i + mid, order);
+		merge(arr, low, mid, order);
+		merge(arr, low + mid, mid, order);
+	}
+}
+
+/**
+ * bitonicsort - this is Bitonic Sort Algorithm to be Implemented.
+ *
+ * @arr: an array
+ * @low: 1st Element
+ *
+ * @nelemnt: the Number Of Elements.
+ *
+ * @order: 0 Descending Order, 1: an ascending Order,
+ *
+ * @size: an Array Lenght
+ */
+
+void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
+{
+	int mid;
+
+	if (nelemnt > 1)
+	{
+		if (order >= 1)
+		{
+			printf("Merging [%i/%i] (UP):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+		else
+		{
+			printf("Merging [%i/%i] (DOWN):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+		mid = nelemnt / 2;
+		bitonicsort(arr, low, mid, 1, size);
+		bitonicsort(arr, low + mid, mid, 0, size);
+		merge(arr, low, nelemnt, order);
+		if (order <= 0)
+		{
+			printf("Result [%i/%i] (DOWN):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+		if (order >= 1)
+		{
+			printf("Result [%i/%i] (UP):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+	}
+}
+
+/**
+ * bitonic_sort - to Prepare Terrain To Bitonic Sort Algorithm.
+ *
+ * @array: an array
+ *
+ * @size: an array lenght.
+ */
+
+void bitonic_sort(int *array, size_t size)
+{
+	int order = 1;
+
+	if (!array || size < 2)
+		return;
+	bitonicsort(array, 0, size, order, size);
+}
